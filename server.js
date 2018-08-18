274afd9eb8c1714a10a5accece503347a2d411ca
Maneesh Karna <makarna@microsoft.com>
2018-08-18 02:00:20 -0700

    set up start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.port || 3000;
 
 const app = express();
 
@@ -54,4 +55,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);   
+app.listen(port, () => {
+    console.log('Server is up and running');
+});   
